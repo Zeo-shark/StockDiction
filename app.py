ab88cf66a04789ceccdd65297d2a8313113e4a2f
Zeo-shark <sauravberaiit@gmail.com>
2021-08-07 19:28:22 +0530

    Added Flask microservices framework configurations

--- app.py
+++ app.py
@@ -454,3 +454,10 @@ def makeWebhookResult(data, req, stock_symbol):
   #     }
   #   ]
   # }
+
+  if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+
+    print "Starting app on port %d" % port
+
+    app.run(debug=False, port=port, host='0.0.0.0')

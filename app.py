cfca6fd3a65c78c591549f29c514d131ace6236f
Zeo-shark <sauravberaiit@gmail.com>
2021-08-07 19:19:27 +0530

    added main functions for calculations of stock prices and determining the dividend

--- app.py
+++ app.py
@@ -122,3 +122,111 @@ def logMessage(req):
     print response
     print response.content 
     print "Success"
+
+def getWelcome(req):
+    response = 'Hi! I am here to help predict financial markets. My predictions are not 100% accurate!'
+    return response
+
+# analyze feelings intent
+def getTwitterFeelings(req):
+    result = req.get("result")
+    parameters = result.get("parameters")
+    stock_symbol = parameters.get("stock_symbol")
+    if stock_symbol is None:
+        return None
+
+    twitter_analyzer = twitter_analyze()
+    twitter_data = twitter_analyzer.analyze_feelings(stock_symbol)
+    print 'Twitter data:'
+    print twitter_data
+
+    data = {}
+    data['positive'] = twitter_data[0]
+    data['negative'] = twitter_data[1]
+    data['neutral'] = twitter_data[2]
+
+    total = data['positive'] + data['negative'] + data['neutral']
+
+    positive_percent = percentage(data['positive'], total)
+    negative_percent = percentage(data['negative'], total)
+    neutral_percent = percentage(data['neutral'], total)
+
+    data_string = 'positive: ' + str(positive_percent) + '% negative: ' + str(negative_percent) + '% neutral: ' + str(neutral_percent) + '%'
+
+    return data_string
+
+# make percentage and round
+def percentage(part, whole):
+    return round(100 * float(part)/float(whole), 2)
+
+# for intent prediction
+def getStockPrediction(req):
+    result = req.get("result")
+    parameters = result.get("parameters")
+    stock_symbol = parameters.get("stock_symbol")
+
+    time = parameters.get("date-period")
+
+    if stock_symbol is None:
+        return None
+
+    num_of_days = 3
+    if time != '' and time is not None:
+        num_of_days = extract_days(time)
+
+    prediction = predictStocks()
+    predicted_values = prediction.stocksRegression(stock_symbol, int(num_of_days))
+    predicted_list = predicted_values.tolist()
+    clean_list = cleanPrediction(predicted_list)
+
+    return '\n'.join(str(v) for v in clean_list)
+
+def cleanPrediction(list_prices):
+    clean_list = []
+
+    for price in list_prices:
+        price_float = float(price[0])
+        str_price =  '%.2f' % price_float
+        clean_list.append(str_price)
+
+    return clean_list
+
+# invest or not
+def getStockClassification(req):
+    result = req.get("result")
+    parameters = result.get("parameters")
+    stock_symbol = parameters.get("stock_symbol")
+
+    time = parameters.get("date-period")
+
+    if stock_symbol is None:
+        return None
+
+    num_of_days = 14
+    if time != '' and time is not None:
+        num_of_days = extract_days(time)
+
+    prediction = predictStocks()
+    
+    predicted_values = prediction.stocksNeuralNet(stock_symbol, int(num_of_days))
+    predicted_decision = predicted_values.tolist()[-1][0]
+    if time != '' and time is not None:
+        return predicted_decision.lower() + ' (decision for next ' + str(num_of_days-1) + ' days)' 
+
+    return predicted_decision.lower() + ' (decision for next two weeks)'
+
+def extract_days(time):
+    num_days = 3
+    dates = time.split('/')
+
+    if dates is not None:
+        first = datetime.strptime(dates[0], "%Y-%m-%d").date()
+        second = datetime.strptime(dates[1], "%Y-%m-%d").date()
+        num_days = (second - first).days+1
+    else:
+        dates = time.split(' ')
+        if dates is not None:
+            if dates[0].isdigit():
+                num_days = int(dates[0])
+
+    return num_days

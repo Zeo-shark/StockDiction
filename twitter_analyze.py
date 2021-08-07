08b024472d6818aeccaf093b3d17a5833823bb18
Zeo-shark <sauravberaiit@gmail.com>
2021-08-07 20:23:34 +0530

    analyze_feelings using tweets

--- twitter_analyze.py
+++ twitter_analyze.py
@@ -74,3 +74,69 @@ class twitter_analyze:
 		#         writer.writerow([val])  
 
 		return counts_list
+
+    def analyze_stock(self, stock):
+		all_tweets = self.get_tweets(stock)
+		tweets = pd.DataFrame()
+		analysis_list = []
+		polarity_list = []
+		subjectivity_list = []
+		tweet_text = []
+		tweet_dates = []
+		for tweet in all_tweets:
+			tweet_text.append(tweet.text.encode("utf-8"))
+			analysis = TextBlob(tweet.text)
+			# prints-Sentiment(polarity=0.0, subjectivity=0.0), polarity is how positive or negative, subjectivity is if opinion or fact
+			# analysis_list.append('polarity:' + str(analysis.se 1ntiment.polarity) + ' subjectivity:' + str(analysis.sentiment.subjectivity))
+			polarity_list.append(str(analysis.sentiment.polarity))
+			subjectivity_list.append(str(analysis.sentiment.subjectivity))
+			tweet_dates.append(tweet.created_at)
+
+		tweets['text'] = np.array(tweet_text)
+		# tweets['analysis'] = np.array(analysis_list)
+		tweets['polarity'] = np.array(polarity_list)
+		tweets['subjectivity'] = np.array(subjectivity_list)
+		tweets['date'] = np.array(tweet_dates)
+		# tweets = tweets.sort_values(by=['subjectivity'], ascending=0)
+		print tweets
+		# tweets.to_csv('data/%s_tweets.csv' % stock)
+		return tweets
+
+	def get_tweets(self, stock):
+		alltweets = []  
+		public_tweets = api.search(stock)
+		alltweets.extend(public_tweets)
+		oldest = alltweets[-1].id - 1
+
+		# Todo date constraint?
+
+		#keep grabbing tweets until there are no tweets left to grab
+		while len(public_tweets) > 0:
+		    print "getting tweets before %s" % (oldest)
+		    # filter by users too, todo
+		    public_tweets = api.search(stock,count=200,max_id=oldest)
+		    
+		    #save most recent tweets
+		    alltweets.extend(public_tweets)
+		    
+		    #update the id of the oldest tweet less one
+		    oldest = alltweets[-1].id - 1
+		    
+		    print "...%s tweets downloaded so far" % (len(alltweets))
+
+		    if len(alltweets) > 500:
+		    	break
+
+		#transform the tweepy tweets into a 2D array that will populate the csv 
+		outtweets = [[tweet.id_str, tweet.created_at, tweet.text.encode("utf-8")] for tweet in public_tweets]
+		print outtweets
+		return alltweets
+
+if __name__ == "__main__":
+	analyze = twitter_analyze()
+	# analyze.analyze_stock('$AAPL')
+	print analyze.analyze_feelings('$TSLA')
+	# analyze.analyze_feelings('$AAPL')
+	# analyze.analyze_feelings('$GOOGL')
+
+

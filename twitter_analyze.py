9fcb9de5bf2db3667460ceb913ce1733f3ebf2ef
Zeo-shark <sauravberaiit@gmail.com>
2021-08-07 19:52:41 +0530

    add api token

--- twitter_analyze.py
+++ twitter_analyze.py
@@ -15,3 +15,10 @@ import numpy as np
 # from pylab import *
 import os.path
 
+access_token = "301847288-lWXEQAwNc7kvyIF4E6w3TCzj7FfWYyUs2FKXbkcR"
+access_token_secret = "dXv1ktTNVsHVHsx7AUyVilLOx3tEWPc0Ffi8BvSh9VN10"
+consumer_key = "MyrxJJIAAbIupjvNbqyUTzJOZ"
+consumer_secret = "ZBZrMl7jEv1DGt76hCV60K7j8Z8uDx8K710cO1w6SBelNVSeqD"
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)

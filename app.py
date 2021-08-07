b7a000e43b073c388cdd91576112e75da86ce9a9
Zeo-shark <sauravberaiit@gmail.com>
2021-08-07 19:17:57 +0530

    added webhooks funtion definitions

--- app.py
+++ app.py
@@ -11,3 +11,60 @@ from yahoo_finance import Share
 from datetime import datetime, timedelta
 import requests
 # import mysql.connector
+
+app = Flask(__name__)
+
+# cnx = mysql.connector.connect(user=os.environ['JW_USERNAME'], password=os.environ['JW_KEY'], host=os.environ['JW_HOST'], database='xcqk05aruwtw0kew')
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    print("Request:")
+    print(json.dumps(req, indent=4))
+
+    res = processRequest(req)
+
+    res = json.dumps(res, indent=4)
+    print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    print r
+    return r
+
+def processRequest(req):
+    result = req.get("result")
+    parameters = result.get("parameters")
+    stock_symbol = parameters.get("stock_symbol")
+
+    # logMessage(req)
+
+    if req.get("result").get("action") == "CurrentPrice.price":   
+        res = makeWebhookResult(getStockCurrentPrice(req), req, stock_symbol)
+        return res
+    elif req.get("result").get("action") == "Prediction.stockForecast":
+        res = makeWebhookResult(getStockPrediction(req), req, stock_symbol)
+        return res 
+    elif req.get("result").get("action") == "Feelings.analyze":
+        res = makeWebhookResult(getTwitterFeelings(req), req, stock_symbol)
+        return res
+    elif req.get("result").get("action") == "DividendDate.Date":
+        res = makeWebhookResult(getStockDividendPayDate(req), req, stock_symbol)
+        return res
+    elif req.get("result").get("action") == "Stock.info":
+        res = makeWebhookResult(getStockInfo(req), req, stock_symbol)
+        return res
+    elif req.get("result").get("action") == "Stock.historical":
+        res = makeWebhookResult(getHistoricalData(req), req, stock_symbol)
+        return res
+    elif req.get("result").get("action") == "Decision.Classification":
+        res = makeWebhookResult(getStockClassification(req), req, stock_symbol)
+        return res 
+    elif req.get("result").get("action") == "input.welcome":
+        res = makeWebhookResult(getWelcome(req), req, stock_symbol)
+        return res
+    elif req.get("result").get("action") == "Visualize.chart":
+        res = makeWebhookResult(getChartURL(req), req, stock_symbol)
+        return res
+    else:
+        return {}
